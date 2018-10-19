4f6b22d931742bba242ce8244054350e3cbae780
Diogo Cezar <diogo@diogocezar.com>
2018-10-19 20:04:54 -0300

    Publicação

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import Button from './components/Button'
 
-const App = function(){
-  return (
-    <Fragment>
-      <h1>Todo List</h1>
-      <ul>
-        <li>Lavar a roupa <Button type="remove">X</Button></li>
-        <li>Jogar LOL <Button type="remove">X</Button></li>
-        <li>Estudar para prova de web <Button type="remove">X</Button></li>
-      </ul>
-      <input type="text" id="todo_item" placeholder="Digite uma tarefa"/>
-      <Button type="add">Adicionar</Button>
-    </Fragment>
-  )
+class App extends Component{
+  state = {
+    todo_item: '',
+    todos : ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      todo_item : e.target.value
+    })
+  }
+  remove = (item) => {
+    if(item){
+      this.setState({
+        todos : this.state.todos.filter((itemList) => {
+          return itemList !== item
+        })
+      })
+    }
+  }
+  add = () => {
+    if(this.state.todo_item){
+      this.setState({
+        todo_item : '',
+        todos : [...this.state.todos, this.state.todo_item]
+      })
+    }
+  }
+  componentDidMount(){
+    // Poderia chamar de uma API
+    setTimeout(() => {
+      this.setState({
+        todos: [
+          'Lavar a roupa',
+          'Jogar LOL',
+          'Estudar para a prova de Web'
+        ]
+      })
+    }, 4000)
+  }
+  render(){
+    return (
+      <Fragment>
+        { this.state.todos &&
+        (
+          <Fragment>
+            <h1>Todo List</h1>
+            <ul>
+              {this.state.todos.map((item, index) => {
+                return <li key={index}>{item} <Button onClick={() => {this.remove(item)}} type="remove">X</Button></li>
+              })}
+            </ul>
+            <input type="text" value={this.state.todo_item} id="todo_item" placeholder="Digite uma tarefa" onChange={this.handleChange}/>
+            <Button type="add" onClick={this.add}>Adicionar</Button>
+          </Fragment>
+        )
+        || <h1>Carregando...</h1>}
+      </Fragment>
+    )
+  }
 }
 
 export default App
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './button.css'
 
 const Button = (props) => {
-    return <button type="button" className={props.type}>{props.children}</button>
+    return <button type="button" onClick={props.onClick} className={props.type}>{props.children}</button>
 };
 
 export default Button;
