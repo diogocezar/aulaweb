3f67da20b7ddc413dc264d5cc1d7c1fbb3efd8e4
Diogo Cezar <diogo@diogocezar.com>
2018-10-19 20:05:44 -0300

    Updates

---
+++ precache-manifest.7274fcd90567c3148084ff168454833f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "266781e6813043fd2498",
+    "url": "./static/js/main.266781e6.chunk.js"
+  },
+  {
+    "revision": "94c8180847106eb35c9d",
+    "url": "./static/js/1.94c81808.chunk.js"
+  },
+  {
+    "revision": "266781e6813043fd2498",
+    "url": "./static/css/main.6718c226.chunk.css"
+  },
+  {
+    "revision": "ec517eb3fe1db11af7796ec589a46d33",
+    "url": "./index.html"
+  }
+];
